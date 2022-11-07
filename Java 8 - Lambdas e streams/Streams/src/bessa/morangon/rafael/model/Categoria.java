c5ad8c6067e9805eb403468d8306c4da0169fd8a
Rafael Bessa <rafaelbessacivil@gmail.com>
2022-11-07 18:47:32 -0300

    Novos exemplos utilizando api de streams

---
+++ Java 8 - Lambdas e streams/Streams/src/bessa/morangon/rafael/model/Categoria.java
@@ -0,0 +1,22 @@
+package bessa.morangon.rafael.model;
+
+
+public class Categoria {
+
+	private String nome;
+
+	public Categoria(String nome) {
+		super();
+		this.nome = nome;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+	
+	@Override
+	public String toString() {
+		return getNome();
+	}
+
+}
